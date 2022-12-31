fef3223601072a5f85fc983b8728c7410a55e1e5
Guillermo A. Diarte <Guillermo_diarte@hotmail.com>
2022-12-31 15:25:14 +0000

    se implementó estilo con ReactBoostrap

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 
 const FormularioComponent = () => {
 
@@ -8,18 +9,44 @@ const FormularioComponent = () => {
     e.preventDefault();
     let datos = e.target;
     let usuario = {
-      nombre: datos.name.value,
-      apellido: datos.lastname.value,
-      genero: datos.sex.value,
-      biografia: datos.bio.value
+      nombre: datos.nombre.value,
+      apellido: datos.apellido.value,
+      genero: datos.sexo.value,
+      biografia: datos.biografia.value
     };
     alert(usuario.nombre + " " + usuario.apellido + " " + usuario.genero + " " + usuario.biografia);
+  
   }
 
   return (
     <div>
       <h1>Formulario con React</h1>
 
+      <Form onSubmit={conseguirDatosFormulario}>
+        <Form.Group className="mb-3" controlId="nombre">
+          <Form.Control type="text" placeholder="Ingrese su Nombre" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="apellido">
+          <Form.Control type="text" placeholder="Ingrese su Apellido" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sexo">
+          <Form.Select className='mb-3'>
+            <option value="hombre">Hombre</option>
+            <option value="mujer">Mujer</option>
+          </Form.Select>
+        </Form.Group>
+        <InputGroup>
+          <Form.Control as="textarea" aria-label="With textarea" id="biografia" />
+        </InputGroup>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+
+      {/*
+      //Codigo sin ReactBoostrap
+      <br/><br/>
       <form onSubmit={conseguirDatosFormulario}>
         <input type="text" name="name" placeholder='Nombre'></input>
         <input type="text" name="lastname" placeholder='Apellido'></input>
@@ -28,8 +55,8 @@ const FormularioComponent = () => {
           <option value="woman">Mujer</option>
         </select>
         <textarea name="bio" placeholder='Biografia'></textarea>
-        <input type='submit' value="Enviar" />
-      </form>
+        <Button type="submit">Enviar</Button>
+      </form>*/}
     </div>
   )
 }
