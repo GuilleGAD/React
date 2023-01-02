0a5e0054b9978b264cb32d88ab258de9360b8913
Guillermo A. Diarte <Guillermo_diarte@hotmail.com>
2023-01-02 00:59:36 +0000

    formulario reactivo

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 const FormularioComponent = () => {
 
-  const [usuario, setUsuario] = useState("");
+  const [usuario, setUsuario] = useState({});
 
   const conseguirDatosFormulario = e => {
     e.preventDefault();
@@ -11,35 +12,69 @@ const FormularioComponent = () => {
     let usuario = {
       nombre: datos.nombre.value,
       apellido: datos.apellido.value,
-      genero: datos.sexo.value,
-      biografia: datos.biografia.value
+      genero: datos.genero.value,
+      biografia: datos.biografia.value,
+      enviar: datos.enviar.name
     };
-    alert(usuario.nombre + " " + usuario.apellido + " " + usuario.genero + " " + usuario.biografia);
-  
+    /*alert(usuario.nombre + " " + usuario.apellido + " " + usuario.genero + " " + usuario.biografia);*/
+    setUsuario(usuario);
   }
+
+  const modUsuario = e => {
+    let name_input = e.target.id;
+    let usuario_para_modificar = usuario;
+
+    //usuario_para_modificar[name_input] = e.target.value;
+
+    setUsuario(estado_previo => {
+      return {
+        ...estado_previo,
+        [name_input]: e.target.value
+      }
+    });
+    
+  };
 
   return (
     <div>
       <h1>Formulario con React</h1>
 
+      {usuario.enviar &&
+        (
+          <Card
+            bg='info'
+            key='info'
+            text='dark'
+            style={{ width: '18rem' }}
+            className="mb-2"
+          >
+            <Card.Body>
+              {usuario.nombre} {usuario.apellido} es un {usuario.genero}
+              <p>y su biografía es la siguiente:</p>
+              <p>{usuario.biografia}</p>
+            </Card.Body>
+          </Card>
+        )
+      }
+
       <Form onSubmit={conseguirDatosFormulario}>
         <Form.Group className="mb-3" controlId="nombre">
-          <Form.Control type="text" placeholder="Ingrese su Nombre" />
+          <Form.Control type="text" onChange={modUsuario} placeholder="Ingrese su Nombre" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="apellido">
-          <Form.Control type="text" placeholder="Ingrese su Apellido" />
+          <Form.Control type="text" onChange={modUsuario} placeholder="Ingrese su Apellido" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="sexo">
-          <Form.Select className='mb-3'>
+        <Form.Group className="mb-3" controlId="genero">
+          <Form.Select className='mb-3' >
             <option value="hombre">Hombre</option>
             <option value="mujer">Mujer</option>
           </Form.Select>
         </Form.Group>
         <InputGroup>
-          <Form.Control as="textarea" aria-label="With textarea" id="biografia" />
+          <Form.Control as="textarea" aria-label="With textarea" onChange={modUsuario} id="biografia" />
         </InputGroup>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button variant="primary" type="submit" name="enviar">
+          Enviar
         </Button>
       </Form>
 
